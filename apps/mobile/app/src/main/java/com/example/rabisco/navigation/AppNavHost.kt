c705f7043f36b95737985852b9475ce84c489aa0
daniel christopher <daniel.2005.christopher@gmail.com>
2025-11-19 11:14:18 -0300

    feat: setup routes with nav compose

---
+++ apps/mobile/app/src/main/java/com/example/rabisco/navigation/AppNavHost.kt
@@ -0,0 +1,27 @@
+package com.example.rabisco.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.rabisco.ui.screens.auth.AuthScreen
+import com.example.rabisco.ui.screens.home.HomeScreen
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.Auth.path
+    ) {
+        composable(Routes.Auth.path) {
+            AuthScreen(navigator = navController)
+        }
+
+        composable(Routes.Home.path) {
+            HomeScreen()
+        }
+    }
+}
