231d7e15b6f189e5f0681dec33af34e0436fa601
daniel christopher <daniel.2005.christopher@gmail.com>
2025-11-25 21:26:07 -0300

    chore: enable mobile access

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   );
   app.useGlobalGuards();
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
