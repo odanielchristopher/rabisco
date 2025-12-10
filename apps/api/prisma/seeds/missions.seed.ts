0697ed207f71e04ee0e2f414f9376798072ab021
daniel christopher <daniel.2005.christopher@gmail.com>
2025-12-10 10:29:16 -0300

    feat: update achievements seeds

--- apps/api/prisma/seeds/missions.seed.ts
+++ apps/api/prisma/seeds/missions.seed.ts
@@ -3,11 +3,10 @@ import { MISSIONS } from '@shared/constants/missions.constants';
 import { prisma } from './lib/prisma-client.lib';
 
 export async function seedMissions() {
-  for (const mission of MISSIONS) {
-    await prisma.dailyMission.create({
-      data: mission,
-    });
-  }
+  await prisma.dailyMission.createMany({
+    data: MISSIONS,
+    skipDuplicates: true,
+  });
 
   // eslint-disable-next-line no-console
   console.log('✅ Daily missions seeded successfully.');
