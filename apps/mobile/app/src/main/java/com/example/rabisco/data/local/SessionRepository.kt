c96cebb246ae2df80dbc6e4f596d372e70ea1eb1
Kleyton Ferreira <kleytonferreira@alu.ufc.br>
2026-01-07 16:47:17 -0300

    feat: create theme viewmodel for dark mode management

--- apps/mobile/app/src/main/java/com/example/rabisco/data/local/SessionRepository.kt
+++ apps/mobile/app/src/main/java/com/example/rabisco/data/local/SessionRepository.kt
@@ -1,16 +1,23 @@
     package com.example.rabisco.data.local
 
     import android.content.Context
+    import androidx.datastore.preferences.core.booleanPreferencesKey
     import androidx.datastore.preferences.core.edit
     import androidx.datastore.preferences.core.stringPreferencesKey
     import androidx.datastore.preferences.preferencesDataStore
+    import kotlinx.coroutines.flow.Flow
     import kotlinx.coroutines.flow.first
+    import kotlinx.coroutines.flow.map
 
     class SessionRepository(private val context: Context) {
+
+        private val DARK_MODE = booleanPreferencesKey("dark_mode")
 
         private val Context.sessionDataStore by preferencesDataStore("session")
 
         private val TOKEN = stringPreferencesKey("token")
+
+
 
         suspend fun saveToken(token: String) {
             context.sessionDataStore.edit { prefs ->
@@ -25,6 +32,17 @@
         suspend fun clearSession() {
             context.sessionDataStore.edit { prefs ->
                 prefs.clear()
+            }
+        }
+
+        fun observeDarkMode(): Flow<Boolean> {
+            return context.sessionDataStore.data.map { prefs ->
+                prefs[DARK_MODE] ?: false
+            }
+        }
+        suspend fun saveDarkMode(enabled: Boolean) {
+            context.sessionDataStore.edit { prefs ->
+                prefs[DARK_MODE] = enabled
             }
         }
     }
