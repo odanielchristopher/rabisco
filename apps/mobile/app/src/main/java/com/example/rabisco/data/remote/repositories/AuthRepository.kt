503b740f87f80f184281fb3e8566bbe7b9359de9
Daniel Lucas <daniellucasm18@gmail.com>
2026-01-10 14:53:52 -0300

    refactor: integrate AuthRepository with api

--- apps/mobile/app/src/main/java/com/example/rabisco/data/remote/repositories/AuthRepository.kt
+++ apps/mobile/app/src/main/java/com/example/rabisco/data/remote/repositories/AuthRepository.kt
@@ -1,12 +1,10 @@
 package com.example.rabisco.data.remote.repositories
 
 import com.example.rabisco.data.local.SessionRepository
-import com.example.rabisco.data.remote.RetrofitClient
 import com.example.rabisco.data.remote.dto.request.SignInDto
 import com.example.rabisco.data.remote.dto.request.SignUpDto
 import com.example.rabisco.data.remote.dto.response.AuthResponse
 import com.example.rabisco.data.remote.providers.ApiProvider
-import kotlinx.coroutines.delay
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -23,33 +21,27 @@ class AuthRepository(
 ) {
     private val client = ApiProvider.provideUnauthenticated().create(AuthApi::class.java)
 
-    suspend fun signin(body: SignInDto): AuthResponse {
-//        val response = this.client.signin(body)
-//        sessionRepository.saveToken(response.accessToken)
-//        return response;
-        delay(1500) // simula atraso da rede
-
-        val fakeResponse = AuthResponse(
-            accessToken = "mocked-token-123",
-        )
-
-        sessionRepository.saveToken(fakeResponse.accessToken)
-
-        return fakeResponse
+    suspend fun signin(body: SignInDto): Result<AuthResponse> {
+        return try {
+            val response = client.signin(body)
+            sessionRepository.saveToken(response.accessToken)
+            Result.success(response)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
-    suspend fun signup(body: SignUpDto): AuthResponse {
-//        val response = this.client.signup(body)
-//        sessionRepository.saveToken(response.accessToken);
-//        return response;
-        delay(1500)
+    suspend fun signup(body: SignUpDto): Result<AuthResponse> {
+        return try {
+            val response = client.signup(body)
+            sessionRepository.saveToken(response.accessToken)
+            Result.success(response)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 
-        val fakeResponse = AuthResponse(
-            accessToken = "mocked-token-abc",
-        )
-
-        sessionRepository.saveToken(fakeResponse.accessToken)
-
-        return fakeResponse
+    suspend fun logout() {
+        sessionRepository.clearSession()
     }
 }
